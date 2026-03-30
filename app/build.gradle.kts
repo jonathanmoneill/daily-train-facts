da2ad2cdd8dca2985b2b2d52fcfac674b6f3b47d
Jonathan O'Neill <jonathanmoneill@gmail.com>
2026-03-30 22:07:57 +0100

    Update dependencies and create new build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gfd.dailytrainfacts"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
